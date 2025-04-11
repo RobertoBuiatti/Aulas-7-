f0ce34e4d13abf0dd336d36f4cdeabb0ee1fefad
Roberto <robertobuiatti2@gmail.com>
2025-04-11 20:42:20 -0300

    aula 11-04 antes do intervalo

--- Jogos digitais/jogo/scenes/fruit.gd
+++ Jogos digitais/jogo/scenes/fruit.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+class_name Fruit
+
+#Fruta comida
+signal fruit_eaten
 
 func _ready() -> void:
 	pass
@@ -8,5 +12,5 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("pegou fruta")
+	fruit_eaten.emit()#emitindo o sinal
 	queue_free() #remove a fruta
--- Jogos digitais/jogo/scenes/level_0.gd
+++ Jogos digitais/jogo/scenes/level_0.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+@onready var player: CharacterBody2D = $Player
+@onready var itens: Node2D = $Itens
+
+var num_fruit:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# percorrer todas as frutas
+	for item in itens.get_children():
+		if item is Fruit:
+			item.fruit_eaten.connect(_on_fruit_eaten)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_fruit_eaten():
+	num_fruit += 1
