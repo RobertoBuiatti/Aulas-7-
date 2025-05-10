d0e3c42681f8ae03458c70981feaf1da5c25ac53
Roberto <robertobuiatti2@gmail.com>
2025-05-09 21:52:33 -0300

    aula jogos 09-05

---
+++ Jogos digitais/jogo/scenes/Finish.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+class_name Finish
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+signal win
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not animation_player.is_playing() or animation_player.current_animation != "idle":
+		animation_player.play("idle")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	win.emit()
+	print("acabo o level")
+	queue_free()
